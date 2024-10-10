7f058d2298b33f2f615697cd25b4163a106f25ed
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-10-10 05:21:19 +0100

    made hero section responsive

--- src/components/footwear/Hero.tsx
+++ src/components/footwear/Hero.tsx
@@ -1,18 +1,22 @@
 const Hero = () => {
   return (
-    <div className="py-[10vh] flex justify-between items-center">
-      <div className="w-[45%]">
-        <h2 className="font-bold text-[64px] uppercase">
+    <div className="lg:py-[10vh] pt-2 pb-10 flex lg:flex-row flex-col-reverse justify-between items-center">
+      <div className="lg:w-[45%]">
+        <h2 className="font-bold lg:text-[64px] text-[32px] uppercase">
           Shop quality <br /> footwears.
         </h2>
-        <p className="text-[#808080] mt-10">
+        <p className="text-[#808080] lg:mt-10 mt-2">
           Browse through our diverse range of meticulously crafted garments,
           designed to bring out your individuality and cater to your sense of
           style.
         </p>
-        <button className="w-[40%] bg-[#000] text-[#fff] mt-6 py-2 rounded-lg">Shop Now</button>
+        <div className="flex lg:justify-start justify-center">
+          <button className="bg-[#000] lg:w-[40%] w-[70%] text-[#fff] lg:mt-6 mt-8 py-2 rounded-lg">
+            Shop Now
+          </button>
+        </div>
       </div>
-      <div className="w-[50%] flex justify-end">
+      <div className="lg:w-[50%] flex lg:justify-end">
         <img src="/assets/footwear/ft-one.png" alt="footwear-one" />
       </div>
     </div>
