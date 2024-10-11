e32a62c4bbbecd4c981ce8a060ed3107bcdb5efa
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-10-11 06:15:20 +0100

    categories component design completed

--- src/components/footwear/Categories.tsx
+++ src/components/footwear/Categories.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Five from "../stars/Five";
-import { Naira } from "../svgs/extras";
+import { Heart, Naira } from "../svgs/extras";
 
 const Categories = () => {
   const items = [
@@ -107,38 +107,149 @@ const Categories = () => {
     <div>
       <hr className="w-[60vw] mx-auto" />
       <div className="pt-[15vh] w-[90vw] mx-auto">
-        <h2 className="text-[48px] font-bold uppercase">categories</h2>
-        <div className="selector space-x-16 mt-6">
+        <h2 className="lg:text-[48px] text-[32px] font-bold uppercase">
+          categories
+        </h2>
+        <div className="selector space-x-10 mt-6 lg:flex hidden">
           <button
             onClick={() => setActiveTab("sneakers")}
-            className="bg-[#000] text-[#fff] p-2 rounded-lg"
+            className={`${
+              activeTab === "sneakers" && "bg-[#000] text-[#fff]"
+            } p-2 rounded-lg uppercase transition-all duration-300 ease-in-out`}
           >
             Sneakers
           </button>
-          <button className="uppercase" onClick={() => setActiveTab("shoes")}>
+          <button
+            className={`${
+              activeTab === "shoes" && "bg-[#000] text-[#fff]"
+            } p-2 rounded-lg uppercase transition-all duration-300 ease-in-out`}
+            onClick={() => setActiveTab("shoes")}
+          >
             Shoes
           </button>
-          <button className="uppercase" onClick={() => setActiveTab("sports")}>
+          <button
+            className={`${
+              activeTab === "sports" && "bg-[#000] text-[#fff]"
+            } p-2 rounded-lg uppercase transition-all duration-300 ease-in-out`}
+            onClick={() => setActiveTab("sports")}
+          >
             Sports
           </button>
         </div>
 
-        <div className="w-[90vw] flex overflow-x-auto mx-auto justify-between py-[10vh] space-x-4">
+        <div className="w-[90vw] flex overflow-x-auto mx-auto justify-between py-10 lg:py-[10vh] space-x-5">
           {items.map(
             (item) =>
               activeTab === item.category && (
                 <div
                   key={item.id}
-                  className="flex-shrink-0 flex flex-col space-y-1 w-[295px]"
+                  className="flex-shrink-0 flex flex-col space-y-1 w-[320px]"
                 >
+                  <div className="h-[40px] flex lg:hidden mb-10">
+                    {item.id === 1 && (
+                      <h2 className="text-[16px] font-semibold uppercase bg-[#000] text-[#fff] p-2 rounded-lg">
+                        {item.category}
+                      </h2>
+                    )}
+                  </div>
+                  <div className="flex items-center justify-between pb-4">
+                    <p className="font-bold uppercase">Hot</p>
+                    <div className="shadow-xl rounded-full bg-[#fff] p-1">
+                      <Heart />
+                    </div>
+                  </div>
                   <img
                     src={item.productImg}
                     alt={item.name}
-                    className="w-[320px] h-[320px] mb-10"
+                    className="w-[320px] h-[320px]"
                   />
-                  <button className="bg-[#000] text-[#fff] py-4 rounded-xl">
-                    Add to Cart
-                  </button>
+                  <div className="pt-10 w-[100%]">
+                    <button className="bg-[#000] w-[100%] text-[#fff] py-4 rounded-xl">
+                      Add to Cart
+                    </button>
+                  </div>
+                  <div className="pt-4">{item.ratingIcon}</div>
+                  <h2 className="uppercase text-[14px] pt-2">{item.name}</h2>
+                  <p className="flex items-center text-[14px]">
+                    <Naira /> {item.price}
+                  </p>
+                </div>
+              )
+          )}
+        </div>
+
+        <div className="w-[90vw] flex lg:hidden overflow-x-auto mx-auto justify-between py-10 lg:py-[10vh] space-x-5">
+          {items.map(
+            (item) =>
+              item.category === "shoes" && (
+                <div
+                  key={item.id}
+                  className="flex-shrink-0 flex flex-col space-y-1 w-[295px]"
+                >
+                  <div className="h-[40px] flex lg:hidden mb-10">
+                    {item.id === 5 && (
+                      <h2 className="text-[16px] font-semibold uppercase bg-[#000] text-[#fff] p-2 rounded-lg">
+                        {item.category}
+                      </h2>
+                    )}
+                  </div>
+                  <div className="flex items-center justify-between pb-4">
+                    <p className="font-bold uppercase">Hot</p>
+                    <div className="shadow-xl rounded-full bg-[#fff] p-1">
+                      <Heart />
+                    </div>
+                  </div>
+                  <img
+                    src={item.productImg}
+                    alt={item.name}
+                    className="w-[320px] h-[320px]"
+                  />
+                  <div className="pt-10 w-[100%]">
+                    <button className="bg-[#000] w-[100%] text-[#fff] py-4 rounded-xl">
+                      Add to Cart
+                    </button>
+                  </div>
+                  <div className="pt-4">{item.ratingIcon}</div>
+                  <h2 className="uppercase text-[14px] pt-2">{item.name}</h2>
+                  <p className="flex items-center text-[14px]">
+                    <Naira /> {item.price}
+                  </p>
+                </div>
+              )
+          )}
+        </div>
+
+        <div className="w-[90vw] flex lg:hidden overflow-x-auto mx-auto justify-between py-10 lg:py-[10vh] space-x-5">
+          {items.map(
+            (item) =>
+              item.category === "sports" && (
+                <div
+                  key={item.id}
+                  className="flex-shrink-0 flex flex-col space-y-1 w-[295px]"
+                >
+                  <div className="h-[40px] flex lg:hidden mb-10">
+                    {item.id === 9 && (
+                      <h2 className="text-[16px] font-semibold uppercase bg-[#000] text-[#fff] p-2 rounded-lg">
+                        {item.category}
+                      </h2>
+                    )}
+                  </div>
+                  <div className="flex items-center justify-between pb-4">
+                    <p className="font-bold uppercase">Hot</p>
+                    <div className="shadow-xl rounded-full bg-[#fff] p-1">
+                      <Heart />
+                    </div>
+                  </div>
+                  <img
+                    src={item.productImg}
+                    alt={item.name}
+                    className="w-[320px] h-[320px]"
+                  />
+                  <div className="pt-10 w-[100%]">
+                    <button className="bg-[#000] w-[100%] text-[#fff] py-4 rounded-xl">
+                      Add to Cart
+                    </button>
+                  </div>
                   <div className="pt-4">{item.ratingIcon}</div>
                   <h2 className="uppercase text-[14px] pt-2">{item.name}</h2>
                   <p className="flex items-center text-[14px]">
