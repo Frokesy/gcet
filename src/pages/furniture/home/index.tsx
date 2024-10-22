aa3700414673faf3545fd1976c36aedc43abab00
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-10-22 23:54:57 +0100

    designed reviews component for furniture template

--- src/pages/furniture/home/index.tsx
+++ src/pages/furniture/home/index.tsx
@@ -2,6 +2,7 @@ import Container from "../../../components/defaults/Container"
 import Categories from "../../../components/furniture/Categories"
 import Hero from "../../../components/furniture/Hero"
 import NewArrivals from "../../../components/furniture/NewArrivals"
+import Reviews from "../../../components/furniture/Reviews"
 import TopSelling from "../../../components/furniture/TopSelling"
 
 const FurnitureHome = () => {
@@ -11,6 +12,7 @@ const FurnitureHome = () => {
         <Categories />
         <NewArrivals />
         <TopSelling />
+        <Reviews />
     </Container>
   )
 }
