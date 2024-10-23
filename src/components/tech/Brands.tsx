a121a8a81cc492876386190c59abb532aee3603a
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-10-23 16:42:08 +0100

    designed Brands component for tech template

---
+++ src/components/tech/Brands.tsx
@@ -0,0 +1,33 @@
+import {
+  Acer,
+  Apple,
+  Dell,
+  Hp,
+  Huawei,
+  Samsung,
+  Sony,
+} from "../svgs/brandIcons";
+
+const Brands = () => {
+  return (
+    <div className="bg-[#fff] py-10">
+      <h2 className="text-center text-[#667085] font-semibold uppercase">
+        Available brands
+      </h2>
+
+      <div className="w-[90vw] mx-auto grid grid-cols-3 gap-6 lg:gap-0 place-items-center lg:flex lg:flex-row lg:justify-between justify-center items-center pt-10">
+        <Hp />
+        <Apple />
+        <Samsung />
+        <Huawei />
+        <Sony />
+        <Dell />
+        <div className="col-span-3">
+          <Acer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Brands;
--- src/pages/tech/home/index.tsx
+++ src/pages/tech/home/index.tsx
@@ -1,10 +1,12 @@
 import Container from "../../../components/defaults/Container"
+import Brands from "../../../components/tech/Brands"
 import Hero from "../../../components/tech/Hero"
 
 const TechHome = () => {
   return (
     <Container>
         <Hero />
+        <Brands />
     </Container>
   )
 }
