4fabd4887b506d56b7b8d5c001e8fae77be4331e
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-10-23 19:57:43 +0100

    designed BestSellers coponent for sports template

--- src/pages/sports/home/index.tsx
+++ src/pages/sports/home/index.tsx
@@ -1,10 +1,12 @@
 import Container from "../../../components/defaults/Container"
+import BestSellers from "../../../components/sports/BestSellers"
 import Hero from "../../../components/sports/Hero"
 
 const SportsHome = () => {
   return (
     <Container>
       <Hero />
+      <BestSellers />
     </Container>
   )
 }
